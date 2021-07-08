31ee59f3388aa2b8b97bddc2d767ee231523dbfa
Vikas yadav <52526748+yadav-vikas@users.noreply.github.com>
2021-07-08 19:27:07 +0530

    Life expectency for a given population
    Bays's rule problems

---
+++ Probability/Q87_life_expectency.py
@@ -0,0 +1,20 @@
+Q. Suppose you're analyzing a population of 100,000 people, and you're trying to understand life expectancy. 
+ Within this population of 100,000 people, 75% can expect to live to the age of 70, 
+ while 45% can expect to live to age 80. Given that a person is 70, what is the probability that they live to the age 80?  
+
+>>>>
+Let S be the event that life expectency of 100k people is 75% who lives upto 70,
+
+Let E be the event that life expectency of 100k people is 45% who lives upto 80,
+
+By Baye's Rule      P(E|S) = P(S|E) x P(E) / P(S)
+
+as given that whoever lives upto age 80 is under 70+ category as well,
+ 
+  so P(S|E) = 1
+  
+- Given that a person is 70, what is the probability that they live to the age 80 will be   P(E|S) 
+
+  P(E|S)  = (1 x (45/100)) / (75/100) = 0.6
+  
+  So there is 60% chance that a person who is 70 will live to the age of 80.   
